3bb85bd14be02a05373b9ab70e9793af617def6d
Bert Laverman <bert.laverman@axoniq.io>
2020-04-17 11:01:30 +0200

    Added README in image dir

--- 2-docker/0-ee-docker-image/README.md
+++ 2-docker/0-ee-docker-image/README.md
@@ -1,2 +1,7 @@
 ## Building an Axon Server EE image
 
+For Axon Server EE we need to build an image, as there is not public one. The `Dockerfile` used here expects the JAR files in the build directory, so I added a script to copy them from the base directory of this repo. It expects Axon Server EE to be in a file named "`axonserver-ee.jar`", and the CLI in a file named "`axonserver-cli.jar`".
+
+To build the image as "`axonserver-ee:running`", just run "`./build-image.sh`". For a custom tag, pass it as parameter, e.g. "`./build-image.sh repo/image:version`".
+
+**Note** This `Dockerfile` already includes both the use of username "axonserver" and user-id `1001`.
--- 2-docker/0-ee-docker-image/build-image.sh
+++ 2-docker/0-ee-docker-image/build-image.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-cp ../../axonserver-ee.jar build/axonserver.jar
-cp ../../axonserver-cli.jar build/axonserver-cli.jar
+TAG=
+TAG_DEF=axonserver-ee:running
+JAR_EE=../../axonserver-ee.jar
+JAR_CLI=../../axonserver-cli.jar
+
+if [[ $# == 1 ]] ; then
+    TAG=$1
+    shift
+else
+    TAG=${TAG_DEF}
+fi
+if [ ! -s ${JAR_EE} ] ; then
+    echo "No EE JARfile found at \"${JAR_EE}\"."
+    exit 1
+fi
+if [ ! -s ${JAR_CLI} ] ; then
+    echo "No CLI JARfile found at \"${JAR_CLI}\"."
+    exit 1
+fi
+
+echo "Building Axon Server EE image as \"${TAG}\"."
+
+cp ${JAR_EE} build/axonserver.jar
+cp ${JAR_CLI} build/axonserver-cli.jar
 
 cd build
-docker build --tag axonserver-ee:running .
+docker build --tag ${TAG} .
