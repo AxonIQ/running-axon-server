9803b062389ce6f198261bacd4b8b9332840845f
Bert Laverman <bert.laverman@axoniq.io>
2020-05-18 11:46:06 +0200

    Several small cleanups regarding Kubernetes. Started deployment Chart for Helm

--- 3-k8s/2-k8s-ee/create-secrets.sh
+++ 3-k8s/2-k8s-ee/create-secrets.sh
@@ -20,6 +20,6 @@ if [[ $# != 2 ]] ; then
 fi
 sed -e s/__SVC_NAME__/$1/g -e s/__NS_NAME__/$2/g < axonserver.properties.tmpl > axonserver.properties
 
-kubectl create secret generic axonserver-license --from-file=../axoniq.license -n $2
-kubectl create secret generic axonserver-token --from-file=../axonserver.token -n $2
+kubectl create secret generic axonserver-license --from-file=../../axoniq.license -n $2
+kubectl create secret generic axonserver-token --from-file=../../axonserver.token -n $2
 kubectl create configmap axonserver-properties --from-file=axonserver.properties -n $2
