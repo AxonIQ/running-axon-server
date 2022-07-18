8b92453096311805d1cc4c325a3f78da724b4e8a
Bert Laverman <bert.laverman@axoniq.io>
2022-07-18 15:32:23 +0200

    Add removal of plugins to cleanup script

--- 2-docker/2-docker-compose-se/cleanup.sh
+++ 2-docker/2-docker-compose-se/cleanup.sh
@@ -6,6 +6,9 @@ rm -f axonserver*.token axonserver.properties admin.password
 echo "Removing ControlDB files"
 rm -f data/*.db
 
+echo "Removing plugins"
+rm -rf plugins/{bundles,cache}
+
 for ctx in events/* ; do
     if [ -d ${ctx} ] ; then
         echo "- Removing ${ctx}"
