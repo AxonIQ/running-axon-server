70e277a7a0c24a98e771d96fb7bb6757b7548bf6
Bert Laverman <bert.laverman@axoniq.io>
2022-07-18 15:41:34 +0200

    Add plugins volumes to cocker compose example for EE

--- 2-docker/3-docker-compose-ee/cleanup.sh
+++ 2-docker/3-docker-compose-ee/cleanup.sh
@@ -6,6 +6,9 @@ rm -f axonserver*.token axonserver.properties admin.password
 echo "Removing ControlDB files"
 rm -f data?/*.db
 
+echo "Removing plugins"
+rm -rf plugins?/{bundles,cache}
+
 for d in log? events? ; do
     echo "Checking ${d}"
     for ctx in ${d}/* ; do
